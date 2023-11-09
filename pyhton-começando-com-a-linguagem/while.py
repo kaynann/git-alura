417ef2e5310aa33867cbbd70fd8e6858548bb1fd
kaynann <kaynanjunior098@gmail.com>
2023-11-08 21:45:49 -0300

    aprendendo boas práticas com while

---
+++ pyhton-começando-com-a-linguagem/while.py
@@ -0,0 +1,18 @@
+print("=" * 40)
+print("Bem vindo ao jogo da adivinhação!!!")
+print("=" * 40)
+
+numero_secreto = 42
+total_tentativas = 3
+
+while(total_tentativas > 0):
+  numero_chute = int(input("Digite um número: "))
+  print(f'O número digitado é {numero_chute}')
+  if(numero_chute == numero_secreto):
+    print("Parabéns, você acertou!")
+    break
+  elif(numero_chute > numero_secreto):
+    print("Você errou! O número inserido é maior que o número secreto.")
+  elif(numero_chute < numero_secreto):
+    print("Você errou! O número inserido é menor que o número secreto.")
+  total_tentativas = total_tentativas - 1
